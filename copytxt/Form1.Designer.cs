2f8530cd1012dc474762e79581b41374059efebe
nurefendi <nur.efendi@students.amikom.ac.id>
2017-01-19 08:45:53 +0700

    version 1.1

---
+++ copytxt/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace copytxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 46);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 85);
+            this.MinimumSize = new System.Drawing.Size(450, 85);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".:: Copy TXT Ala masfendi ::.";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ copytxt/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace copytxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CurrentLine = File.ReadLines(@namatxt).Skip(LastLineNumber).First();
+            CurrentLine = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(CurrentLine.ToLower());
+            textBox1.Text = CurrentLine;
+
+            this.Text = ".:: Copy TXT Ala masfen ::. | " + namatxt;
+        }
+        string CurrentLine;
+        int LastLineNumber;
+        string namatxt = "ini.txt";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    CurrentLine = File.ReadLines(@namatxt).Skip(LastLineNumber).First();
+                    CurrentLine = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(CurrentLine.ToLower());
+                    textBox1.Text = CurrentLine;
+                    LastLineNumber++;
+                    textBox1.Focus();
+                    textBox1.SelectAll();
+                    textBox1.Copy();
+
+
+                }
+                catch
+                {
+                    SendKeys.Send("z");
+                    Application.Exit();
+
+                }           
+        }
+    }
+}
